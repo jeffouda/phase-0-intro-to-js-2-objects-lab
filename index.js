f14a761a25adb8336f4951c9c45dc81276d51877
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-09-15 14:47:00 +0300

    test compeled

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 // Write your solution in this file!
-let employee = {};
+let employee = {
+  name: "Jeff",
+  streetAddress: "Nairobi",
+};
 
-function updateEmployeeWithKeyAndValue(employeeobj, key, value) {
-  return Object.assign({}, employeeobj, { [key]: value });
+// Step 2: Non-destructive update
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+  return {
+    ...employeeObj,
+    [key]: value,
+  };
 }
+
+// Step 3: Destructive update
 function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
   employeeObj[key] = value;
   return employeeObj;
 }
+
+// Step 4: Non-destructive delete
 function deleteFromEmployeeByKey(employeeObj, key) {
-  const newEmployee = Object.assign({}, employeeObj);
-  delete newEmployee[key];
+  const { [key]: _, ...newEmployee } = employeeObj;
   return newEmployee;
 }
+
+// Step 5: Destructive delete
 function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
   delete employeeObj[key];
   return employeeObj;
 }
+
+// Step 6: Export everything
 module.exports = {
   employee,
   updateEmployeeWithKeyAndValue,
